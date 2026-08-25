c181b7c52b6f23c82b65c655b9638b8098994307
Benyamin <benyaminardestanizadeh@outlook.com>
2026-08-25 16:08:46 +0330

    chore: install tailwind , define design system and sync it with tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,24 @@ import { ThemeToggle } from "@/components/ThemeToggle";
 
 export default function HomePage() {
   return (
-    <main
-      style={{
-        minHeight: "100%",
-        padding: "16px",
-        backgroundColor: "var(--background)",
-        color: "var(--foreground)",
-      }}
-    >
-      <div
-        style={{
-          color: "var(--primary-foreground)",
-          padding: "16px",
-          borderRadius: "8px",
-        }}
-      >
+    <main className="p-4">
+      <div>
         <ThemeToggle />
-        <div style={{ display: "flex", flexDirection: "column", gap: "32px" }}>
+        <div className="flex flex-col gap-6">
           <TextField label="Email" placeholder="Email" />
           <TextArea label="Message" placeholder="Message" />
+          <div className="bg-background text-foreground border border-border p-4">
+            Tailwind is working
+          </div>
+          <div className="bg-primary text-background p-4">Primary</div>
+          <div className="border-4 border-primary bg-background p-4 text-foreground">
+            Border test
+          </div>
+          <div className="bg-primary p-4 text-background">Primary test</div>
+          <div className="bg-secondary p-4 text-foreground">Secondary test</div>
+          <div className="bg-secondary p-4 text-foreground">Secondary test</div>
+
+          <div className="p-4 text-foreground test shadow-xl">TEST</div>
         </div>
       </div>
     </main>
